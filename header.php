5103813491fc6819f18281de7767aea5d1ffdccf
LANKOANDE Urbain <102440329+Lankoande-Urbain@users.noreply.github.com>
2023-10-30 03:25:23 +0000

    Add files via upload

---
+++ header.php
@@ -0,0 +1,25 @@
+<?php
+
+echo '
+   <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav">
+                    <li><a href="#about">Moi</a></li>
+                    <li><a href="#skills">Competence</a></li>
+                    <li><a href="#experience">Maîtrise de Logiciel</a></li>
+                    <li><a href="#education">Education</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>'
+    ?>
+    
